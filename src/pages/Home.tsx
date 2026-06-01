a850db926349e6979b09ece862b4b935dce4a21e
shubham1109 <shubhamshukla1109@gmail.com>
2026-06-01 16:11:54 +0530

    Update Home page: clean imports and remove commented sections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,14 @@
 import Hero from '../sections/Hero';
-import Technology from '../sections/Technology';
 import FilmShowcase from '../sections/FilmShowcase';
-import Impact from '../sections/Impact';
 import Contact from '../sections/Contact';
 
 export default function Home() {
   return (
     <>
       <Hero />
-      {/* <Technology /> */}
+
       <FilmShowcase />
-      {/* <Impact /> */}
+
       <Contact />
     </>
   );
